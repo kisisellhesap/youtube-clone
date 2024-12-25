ef0c62eca4bd7d928580a3fd9087a1ba04e3afe3
kisisellhesap <ahmetcannkaragoz@gmail.com>
2024-12-25 18:08:47 +0300

    update

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,8 @@
 import Header from "../components/Header";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import AsideL from "../components/Main/AsideL";
 import { useEffect } from "react";
 const MainLayout = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/");
-  }, []);
   return (
     <div className="app">
       <Header />
